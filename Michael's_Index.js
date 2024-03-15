bb87bfe653cd6e33e148940314d5a04a8552baea
M3GAalien <agamalianmike@gmail.com>
2024-03-15 12:31:18 -0400

    able to deals on a specific

--- Michael's_Index.js
+++ Michael's_Index.js
@@ -1,5 +1,5 @@
 const url = 'https://www.cheapshark.com/api/1.0/games?id=612'
-
+// able to deals on a specific game
 
 fetch(url)
     .then(response => response.text()) // send response body to next then chain
